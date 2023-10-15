f91006c30e9c05920f0f4bbd0e16e8755138c72f
sergeygap <1ggserongg1@gmail.com>
2023-10-15 20:16:38 +0300

    add splashScreen with checking internet state

--- app/src/main/java/com/gap/sportnews/presentation/MainActivity.kt
+++ app/src/main/java/com/gap/sportnews/presentation/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.gap.sportnews.presentation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -44,5 +46,11 @@ class MainActivity : AppCompatActivity() {
         val constraintLayout = findViewById<ConstraintLayout>(R.id.bottomNavigationView)
         constraintLayout.visibility = View.VISIBLE
         super.onBackPressed()
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
     }
 }
