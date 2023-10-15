a632cedfe9088ad1fa60af664ac78d43681f1222
sergeygap <1ggserongg1@gmail.com>
2023-10-15 19:52:49 +0300

    add cleanString in DetailFragment

--- app/src/main/java/com/gap/sportnews/data/mapper/Mapper.kt
+++ app/src/main/java/com/gap/sportnews/data/mapper/Mapper.kt
@@ -1,14 +1,13 @@
 package com.gap.sportnews.data.mapper
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.text.Html
 import com.gap.sportnews.data.network.model.NewsDto
 import com.gap.sportnews.domain.News
 import java.time.Instant
-import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
-import java.util.Calendar
-import java.util.Locale
 
 class Mapper {
     fun mapDtoEntity(dto: NewsDto): News {
@@ -25,12 +24,47 @@ class Mapper {
     }
 
     private fun deleteFromHTML(content: String): String {
-        return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            Html.fromHtml(content, Html.FROM_HTML_MODE_LEGACY).toString()
+        return if (versionCompat()) {
+            cleanString(
+                Html.fromHtml(content, Html.FROM_HTML_MODE_LEGACY).toString()
+            ).trimStart().trimEnd()
         } else {
-            Html.fromHtml(content).toString()
+            cleanString(Html.fromHtml(content).toString())
+                .trimStart().trimEnd()
         }
     }
+
+    private fun cleanString(content: String): String {
+        val lastIndex = content.lastIndexOf("})});")
+        var finishString = if (lastIndex != -1) {
+            content.substring(lastIndex + 5)
+        } else {
+            content
+        }
+        finishString = truncateStringToWord(finishString)
+        finishString = removeCharacter(finishString)
+        return finishString
+    }
+
+    private fun removeCharacter(input: String): String {
+        val charToRemove = DELETE_CHAR
+        val pattern = "\\s*$charToRemove".toRegex()
+        return input.replace(pattern, "")
+    }
+
+    private fun truncateStringToWord(input: String): String {
+        val wordToFind = FINISH_STRING
+        val index = input.indexOf(wordToFind)
+        return if (index != -1) {
+            input.substring(0, index)
+        } else {
+            input
+        }
+    }
+
+
+    @SuppressLint("ObsoleteSdkInt")
+    private fun versionCompat() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
 
     private fun mapTimeStampToDateTime(postedTime: Long): String {
         val instant = Instant.ofEpochSecond(postedTime)
@@ -43,5 +77,7 @@ class Mapper {
     companion object {
         private const val REGION = "Europe/Moscow"
         private const val DATA_PATTERN = "HH:mm dd.MM.yyyy"
+        private const val FINISH_STRING = "Игра: проверьте свою футбольную орфографию"
+        private const val DELETE_CHAR = '￼'
     }
 }
